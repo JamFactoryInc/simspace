2c964b83287771ec3d08e08129796dfe8bdbd1a5
Jam <sage.jas.smith@gmail.com>
2023-10-15 12:04:39 +1000

    LOG-002-4: fixed bouncing

--- notes.md
+++ notes.md
@@ -11,3 +11,14 @@
 # LOG-002-1:
 ## Notes:
     Looks like I just adjusted the potition to be exactly y=0 instead of making it bounce
+
+# LOG-002-2:
+## Notes:
+    Weird oscellating probably caused by per-frame multiplaction of velocity
+
+# LOG-002-4:
+## Notes:
+    Looks like things are mostly fixed up now thanks to copying the sign of the velocity instead of the negative velocity
+    There's most likely an issue with maintaining the energy of the system since gravity is being applied constantly
+    while in fact for the frame where y <= 0 we aren't falling for the whole frame (because of the bounce adjustment)
+    I think this will cause the energy to slowly increase, or at least not remain constant, but for now it's fine
--- src/ecs/physics.rs
+++ src/ecs/physics.rs
@@ -1,5 +1,6 @@
 use std::ops::{Sub};
 use bevy::prelude::{Component, Deref, DerefMut, Query};
+use godot::log::godot_print;
 use godot::prelude::Vector2;
 
 pub const GRAVITY: Vector2 = Vector2 { x: 0.0, y: 0.98 };
@@ -25,23 +26,24 @@ pub fn physics(mut query: Query<(&mut Position, &mut Velocity)>) {
         // move
         **position += **velocity;
         
-        // adjust for when the velocity step moves past a bound
-        if position.x < CONTAINER.0.x {
-            position.x += 2.0 * CONTAINER.0.x.sub(position.x).copysign(-velocity.x)
-        }
-        if position.x > CONTAINER.1.x {
-            position.x += 2.0 * CONTAINER.1.x.sub(position.x).copysign(-velocity.x)
-        }
-        if position.y < CONTAINER.0.y {
-            position.y += 2.0 * CONTAINER.0.y.sub(position.y).copysign(-velocity.y)
-        }
-        if position.y > CONTAINER.1.y {
-            position.y += 2.0 * CONTAINER.1.y.sub(position.y).copysign(-velocity.y)
-        }
-        
         // bounce
         velocity.x = velocity.x.copysign(velocity.x * (CONTAINER.1.x - position.x) * (position.x - CONTAINER.0.x));
         velocity.y = velocity.y.copysign(velocity.y * (CONTAINER.1.y - position.y) * (position.y - CONTAINER.0.y));
+        
+        godot_print!("before: pos: {:?}, vel: {:?}", **position, **velocity);
+        // adjust for when the velocity step moves past a bound
+        if position.x < CONTAINER.0.x {
+            position.x += 2.0 * CONTAINER.0.x.sub(position.x).copysign(velocity.x)
+        }
+        if position.x > CONTAINER.1.x {
+            position.x += 2.0 * CONTAINER.1.x.sub(position.x).copysign(velocity.x)
+        }
+        if position.y < CONTAINER.0.y {
+            position.y += 2.0 * CONTAINER.0.y.sub(position.y).copysign(velocity.y)
+        }
+        if position.y > CONTAINER.1.y {
+            position.y += 2.0 * CONTAINER.1.y.sub(position.y).copysign(velocity.y)
+        }
         
         // accelerate
         **velocity -= GRAVITY;
